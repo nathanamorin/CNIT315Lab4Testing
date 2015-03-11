5078a39c51f03232cafb234ca00ea0a26e4a1f51
Nathan Morin <nathanamorin@gmail.com>
2015-03-11 10:08:54 -0400

    moved function prototypes to header file

---
+++ lab4_tester.h
@@ -0,0 +1,157 @@
+/* 
+CNIT 315
+Lab 04
+Testing Team
+
+Functions to Test
+􏰃Accept a string from the user.
+􏰃Find the length of a string.
+􏰃Count the number of vowels in a string. [Extra credits if your function uses recursion!] 􏰃 
+Count the number of words in a string. [Extra credits if your function uses recursion!] 􏰃 
+Determine if a character is alpha-numeric. (i.e. it lies between a-z, A-Z, or 0-9)
+􏰃Create a sub-string by specifying the indices.
+􏰃Concatenate two strings.
+􏰃Remove a particular word from the string.
+
+
+Collaborators:
+
+Some functions copied from lab 03
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+Function : Test Find Length
+Purpose: runs test on indicated function
+@return
+0 - Passed All Tests
+-1 - Return Incorrect Result
+-2 - Did Not Return Result
+1 - Failed to Run
+*/
+int testFindLength();
+
+/*
+Function : Test Number Vowels
+Purpose: runs test on indicated function
+@return
+0 - Passed All Tests
+-1 - Return Incorrect Result
+-2 - Did Not Return Result
+1 - Failed to Run
+*/
+int testVowels();
+
+/*
+Function : Test Number Words
+Purpose: runs test on indicated function
+@return
+0 - Passed All Tests
+-1 - Return Incorrect Result
+-2 - Did Not Return Result
+1 - Failed to Run
+*/
+int testNumberWords();
+
+/*
+Function : Test Alpha Numeric Word
+Purpose: runs test on indicated function
+@return
+0 - Passed All Tests
+-1 - Return Incorrect Result
+-2 - Did Not Return Result
+1 - Failed to Run
+*/
+int testAlphaNumericWord();
+
+/*
+Function : Test Substring
+Purpose: runs test on indicated function
+@return
+0 - Passed All Tests
+-1 - Return Incorrect Result
+-2 - Did Not Return Result
+1 - Failed to Run
+*/
+int testSubString();
+
+/*
+Function : Test Concatenation 
+Purpose: runs test on indicated function
+@return
+0 - Passed All Tests
+-1 - Return Incorrect Result
+-2 - Did Not Return Result
+1 - Failed to Run
+*/
+int testConcat();
+
+/*
+Function : Test Remove Word
+Purpose: runs test on indicated function
+@return
+0 - Passed All Tests
+-1 - Return Incorrect Result
+-2 - Did Not Return Result
+1 - Failed to Run
+*/
+int testRemove();
+
+/*
+Function : generate report
+Purpose: using testing functions, generate report and push out to .txt file
+			specified in main input.
+*/
+int generateReport(char* file);
+
+
+
+/**
+* Function: deep_copy_string()
+*
+* Duplicates the input array and stores the deep copy in the output array.
+*
+* Parameters:
+* @param array_from	The array that needs to be copied.
+* @param array_to  	The new array which will hold a deep copy of the input
+* 						array.
+* @param array_length	The length of the input array.
+*
+* Returns:
+* 	@return 	0 - Successful completion of the function.
+* 				1 - An error during operation.
+*
+* Expectations:
+* 	- All parameters are valid, i.e.:
+* 			* array_from is declared.
+*			* array_to has enough memory allocated.
+*			* array_length accurately portrays the number of elements in
+*			  array_from
+* Coppied From Lab 3
+*/
+void deep_copy_string(char* array_from, char* array_to, int array_length);
+
+/**
+* Function: generate_random_string()
+*
+* Generates an integer array of specified size filled with random chars.
+*
+*
+* Parameters:
+* 	@param array 			Pointer to the char array.
+* 	@param array_length 	Number elements to be stored in the array.
+*
+* Returns:
+* 	@return 	0 - Successful.
+* 				1 - Fail/Error.
+*
+* Expectations:
+* 	- None
+* Modified From Lab 3
+*/
+
+int generate_random_string(char* array, int array_length);
+
+int addStrings(char *array_main, char *array_add);
